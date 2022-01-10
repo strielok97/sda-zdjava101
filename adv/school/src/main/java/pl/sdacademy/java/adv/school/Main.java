6a0dee4d8035dc700d340a78abb7aa1f8b62aa30
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2022-01-10 21:14:52 +0100

    Prototyp parsowania linii i Optional

--- adv/school/src/main/java/pl/sdacademy/java/adv/school/Main.java
+++ adv/school/src/main/java/pl/sdacademy/java/adv/school/Main.java
@@ -1,5 +1,6 @@
 package pl.sdacademy.java.adv.school;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -25,11 +27,21 @@ public class Main {
             int lineNumber = 0;
             while(scanner.hasNextLine()){
                 lineNumber++;
-                LOGGER.info("Parsing line [{}]: {}", lineNumber, scanner.nextLine()); //najszybsza metoda korzystania z logera
-
+                String line = scanner.nextLine();
+                if (StringUtils.isBlank(line)) {
+                    LOGGER.info("Skipping line [{}]", lineNumber);
+                    continue;
+                }
+                LOGGER.info("Parsing line [{}]: {}", lineNumber, line);
+                //parseStudent(line).ifPresent(student -> students.add(student));
+                parseStudent(line).ifPresent(students::add);
             }
-
         }
 
     }
+
+    public static Optional<Student> parseStudent(String line) {
+        return Optional.empty();
+    }
+
 }
