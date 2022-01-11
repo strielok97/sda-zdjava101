b1198293b2e5e5bb34d4f2f393637f88caa922fc
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2022-01-11 18:34:59 +0100

    Parsowanie stringa na obiekt Student

--- adv/school/src/main/java/pl/sdacademy/java/adv/school/Main.java
+++ adv/school/src/main/java/pl/sdacademy/java/adv/school/Main.java
@@ -6,10 +6,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.*;
 
 public class Main {
 
@@ -41,7 +39,35 @@ public class Main {
     }
 
     public static Optional<Student> parseStudent(String line) {
-        return Optional.empty();
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter(",");
+        List<String> strings = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String next = scanner.next();
+            strings.add(StringUtils.strip(next, "\""));
+        }
+        if (strings.size() != 10) {
+            LOGGER.warn("Skipped line: {}", line);
+            return Optional.empty();
+        }
+        Student student = new Student();
+        try {
+            student.setId(strings.get(0));
+            student.setLastName(strings.get(1));
+            student.setFirstName(strings.get(2));
+            student.setStartYear(Short.parseShort(strings.get(3)));
+            student.setSchoolYear(Byte.parseByte(strings.get(4)));
+            student.setClassCode(strings.get(5).charAt(0));
+            LocalDate birthDate = LocalDate.of(Integer.parseInt(strings.get(6)), Integer.parseInt(strings.get(7)),
+                    Integer.parseInt(strings.get(8)));
+            student.setBirthDate(birthDate);
+            student.setCity(strings.get(9));
+        } catch (Exception e) {
+            LOGGER.error("Parsing error", e);
+            return Optional.empty();
+        }
+        return Optional.of(student);
+
     }
 
 }
