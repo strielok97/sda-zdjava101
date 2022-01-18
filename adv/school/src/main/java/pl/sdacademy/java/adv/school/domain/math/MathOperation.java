f26a8d4d4ebb3dc6698804b0dc2b7cdb31d1d72f
mient <przemekdziubak@wp.pl>
2022-01-18 21:11:13 +0100

    lambda examples2

--- adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/math/MathOperation.java
+++ adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/math/MathOperation.java
@@ -1,6 +1,6 @@
 package pl.sdacademy.java.adv.school.domain.math;
 
 @FunctionalInterface
-public interface MathOperation {  //z interfejsu funkcyjnego można zrobić lambde, bo ma jedną metode
+public interface MathOperation {  //z interfejsu funkcyjnego można zrobić lambde, bo ma jedną metode1
     double operation (double x, double y);
 }
